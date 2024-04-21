9e38a29ebcbf3eebdd6c25b5e74e56f3e58d1b1e
Guilherme Sousa <guilherme.tec1107@gmail.com>
2024-04-21 14:27:46 -0300

    teste joão

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const form = document.querySelector('form')
+const input = document.querySelector('.input')
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+    console.log('===== flag11 =====')
+    console.log(tab.id)
+
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         function: replaceImages
     })
-
-    
 })
+
+const replaceImages = () => {
+    console.log('===== flag =====')
+
+    const images = documents.querySelectorAll('img')
+    console.log('===== flag =====')
+    images.forEach(image => {
+        image.src = 'as'
+    });
+
+}
+/* 217767661 */

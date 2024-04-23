0173c1f62d16ce23309ba6d5f70d8b75a12c2f63
Guilherme Sousa <guilherme.tec1107@gmail.com>
2024-04-22 22:07:10 -0300

    versão final do db

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     criar um veo tutorial básico com o Marka e usar de dados base para ajudar o usuário
 */
 
-var request = indexedDB.open("Marka", 2);
+var request = indexedDB.open("Marka", 1);
 var db;
 
 request.onsuccess = function(event) {
@@ -17,42 +17,62 @@ request.onerror = function(event) {
 request.onupgradeneeded = function(event) {
   db = event.target.result;
 
-  var objectStore = db.createObjectStore("links", { keyPath: "id", autoIncrement:true });
+  var objectStore = db.createObjectStore("links", { keyPath: "link", autoIncrement:false });
 
   objectStore.createIndex("link", "link", { unique: false });
-  objectStore.createIndex("tempo", "tempo", { unique: false });
-  objectStore.createIndex("descricao", "descricao", { unique: false });
+  objectStore.createIndex("registros", "registros", { unique: false });
 
   console.log("Banco de dados e tabela criados com sucesso");
 };
 
-function adicionarRegistro(link, tempo, descricao) {
-  var transaction = db.transaction(["links"], "readwrite");
-  var objectStore = transaction.objectStore("links");
-  var request = objectStore.add({ link: link, tempo: tempo, descricao: descricao });
-
-  request.onsuccess = function(event) {
-    console.log("Registro adicionado com sucesso");
-  };
-
-  request.onerror = function(event) {
-    console.error("Erro ao adicionar registro", event.target.error);
-  };
+function adicionarRegistro(registro) {
+    var transaction = db.transaction(["links"], "readwrite")
+    var store = transaction.objectStore("links")
+    store.add(registro)
 }
 
-function recuperarRegistros() {
-  var transaction = db.transaction(["links"]);
-  var objectStore = transaction.objectStore("links");
-  var getRequest = objectStore.getAll();
+function mostrarRegistros() {
+    var transaction = db.transaction(["links"], "readonly");
+    var registrosStore = transaction.objectStore("links");
+    var getRequest = registrosStore.getAll();
+  
+    getRequest.onsuccess = function(event) {
+      var registros = event.target.result;
+  
+      // Mostrar os registros no console
+      registros.forEach(function(registro) {
+        console.log(registro);
+      });
+    };
+  
+    getRequest.onerror = function(event) {
+      console.error("Erro ao recuperar registros", event.target.error);
+    };
+  }
 
-  getRequest.onsuccess = function(event) {
-    var registros = event.target.result;
-    registros.forEach(function(registro) {
-      console.log("ID:", registro.id, "Link:", registro.link, "Tempo:", registro.tempo, "Descrição:", registro.descricao);
-    });
-  };
+/* 
+  Código padrão para popular o db para teste
 
-  getRequest.onerror = function(event) {
-    console.error("Erro ao recuperar links", event.target.error);
-  };
-}
+
+[
+  {
+    "link": "http://exemplo1.com",
+    "registros": [
+      { "tempo": "12:22", "descricao": "ele fala sobre o programa llm" },
+      { "tempo": "13:45", "descricao": "apresentação do novo produto" }
+    ]
+  },
+  {
+    "link": "http://exemplo2.com",
+    "registros": [
+      { "tempo": "15:10", "descricao": "reunião com a equipe de desenvolvimento" },
+      { "tempo": "17:30", "descricao": "análise de desempenho trimestral" },
+      { "tempo": "19:00", "descricao": "jantar de confraternização" }
+    ]
+  }
+].forEach(registro =>{
+    adicionarRegistro(registro)
+}) */
+
+
+
